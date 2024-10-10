c6e8657c990d9b7efbbfdedfd4200abdd4dd87aa
Hans-Jörg Bibiko <hans_joerg_bibiko@eva.mpg.de>
2024-10-10 15:23:14 +0200

    some minor improvements, added statistics

--- amazonianvoices/util.py
+++ amazonianvoices/util.py
@@ -1,4 +1,5 @@
 from pyclts.ipachart import VowelTrapezoid, PulmonicConsonants
+from clld import RESOURCES
 
 
 def language_detail_html(context=None, request=None, **kw):
@@ -11,3 +12,9 @@ def language_detail_html(context=None, request=None, **kw):
     res['consonants_html'], res['consonants_css'] = d.render()
     res['uncovered'] = [p for i, p in enumerate(context.inventory) if i not in covered]
     return res
+
+def dataset_detail_html(context=None, request=None, **kw):
+    return dict(
+        stats=context.get_stats(
+            [rsc for rsc in RESOURCES if rsc.name in ['language', 'parameter', 'value']])
+    )
